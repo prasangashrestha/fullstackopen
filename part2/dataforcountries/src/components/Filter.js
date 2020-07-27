83072ec112ff86b71eb9e676f33f1bf2ef4960a1
Prasanga <101890972@student.swin.edu.au>
2020-07-28 05:40:44 +1000

    countries task 2.12

--- part2/dataforcountries/src/components/Filter.js
+++ part2/dataforcountries/src/components/Filter.js
@@ -8,21 +8,36 @@ const Search = ({countries}) => {
     setSearchCountry(e.target.value);
   };
 
+  //filter the country based on the search
   const filterCountry = countries.filter((country) => {
     return country.name.toLowerCase().includes(searchCountry.toLowerCase());
   });
+
+  //rendering results based on the conditions
+  const switchResult = () => {
+    if (filterCountry.length > 10) {
+      return <p>Too many matches, specify another filter</p>;
+    }
+
+    if (filterCountry.length <= 10) {
+      //render the information on the particular filter
+      if (filterCountry.length === 1) {
+        return <Result country={filterCountry[0]} />;
+      } else {
+        //list out all the countries
+        return filterCountry.map((country) => (
+          <p key={country.alpha3Code}>{country.name}</p>
+        ));
+      }
+    }
+  };
 
   return (
     <div>
       <label>find countries</label>
       <input type='text' value={searchCountry} onChange={handleFilter} />
 
-      {filterCountry.length > 10 ? (
-        <p>Too many matches, specify another filter</p>
-      ) : (
-        filterCountry.map((country) => <p key={country.id}>{country.name}</p>)
-      )}
-      <Result />
+      {switchResult()}
     </div>
   );
 };
--- part2/dataforcountries/src/components/Result.js
+++ part2/dataforcountries/src/components/Result.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Result = () => {
-  return <div>Result</div>;
+const Result = ({country: {name, capital, population, languages, flag}}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+
+      <p>capital {capital}</p>
+      <p>population {population}</p>
+
+      <h2>languages</h2>
+      <ul>
+        {languages.map((language) => (
+          <li key={language.iso639_1}>{language.name}</li>
+        ))}
+      </ul>
+
+      <img src={flag} alt={name} />
+    </div>
+  );
 };
 
 export default Result;
